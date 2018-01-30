d24b2c5369a5fa6e107e00bcc53845884703c178
msalvetat <marjorie.salvetat@gmail.com>
2018-01-30 20:05:13 +0100

    Update criteria

--- src/lib/components/aeris-catalog-criteria/aeris-catalog-criteria.vue
+++ src/lib/components/aeris-catalog-criteria/aeris-catalog-criteria.vue
@@ -1,6 +1,11 @@
 <template>
 <div data-aeris-catalog-criteria>
-  <slot></slot>
+  <figure>
+    <figcaption>
+      Criteria
+    </figcaption>
+    <slot></slot>
+  </figure>
 </div>
 </template>
 
@@ -13,30 +18,40 @@ export default {
     lang: {
       type: String,
       default: 'en'
-    },
-    visible: {
-      type: Boolean,
-      default: true
-    },
+    }
   },
-
-  watch: {},
-
-  destroyed: function() {},
-
-  created: function() {},
-
-  mounted: function() {},
-
-  computed: {},
 
   data() {
-    return {}
+    return {
+      aerisThemeListener: null,
+      theme: null
+    }
   },
 
-  updated: function() {},
+  created: function() {
+    console.log("aeris-keyword-search-criteria - Creating");
+    this.aerisThemeListener = this.handleTheme.bind(this)
+    document.addEventListener('aerisTheme', this.aerisThemeListener);
+  },
 
-  methods: {}
+  mounted: function() {
+    var event = new CustomEvent('aerisThemeRequest', {});
+    document.dispatchEvent(event);
+  },
+
+  destroyed: function() {
+    document.removeEventListener('aerisTheme', this.aerisThemeListener);
+    this.aerisThemeListener = null;
+  },
+
+  methods: {
+    handleTheme(e) {
+      this.theme = e.detail;
+      if (this.theme && document.querySelectorAll('[data-aeris-catalog-criteria] .aeris-criterion header h4').length > 0) {
+        document.querySelectorAll('[data-aeris-catalog-criteria] .aeris-criterion header h4').forEach(el => el.style.color = this.theme.primary);
+      }
+    }
+  }
 }
 </script>
 
@@ -48,12 +63,65 @@ export default {
   overflow-y: auto;
 }
 
-[data-aeris-catalog-criteria] span {
-  font-weight: 300;
+[data-aeris-catalog-criteria] figure {
+  margin: 0;
+  padding: 0;
 }
 
-[data-aeris-catalog-criteria] h3 {
-  font-size: 1rem;
-  font-weight: 400;
+[data-aeris-catalog-criteria] figure figcaption {
+  font-weight: 300;
+  font-size: 1.2rem;
+  margin: 12px;
+  color: #FAFAFA;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion {
+  padding: 5px;
+  width: 100%;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion header {
+  display: flex;
+  align-items: center;
+  margin: 8px;
+  cursor: pointer;
+  font-weight: 300;
+  font-size: 1em;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion header aside {
+  display: flex;
+  flex-flow: row nowrap;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion header aside i {
+  margin-right: 8px;
+  transition: 0.3s;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion.showBody header aside i {
+  transform: rotate(90deg);
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion header h4 {
+  margin: 8px;
+  font-weight: 300;
+  font-size: 1.2em;
+  line-height: 1.2;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion header h4 i {
+  margin-right: 8px;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion article {
+  display: none;
+  padding: 10px;
+  word-wrap: break-word;
+}
+
+[data-aeris-catalog-criteria] .aeris-criterion.showBody article {
+  display: block;
+  height: 100%;
 }
 </style>
