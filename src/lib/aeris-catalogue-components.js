e726415acf054108d8fb57ff2db330264d84e2cb
msalvetat <marjorie.salvetat@gmail.com>
2018-10-26 19:57:37 +0200

    build(dependencies): Delete inject in simple entry

--- src/lib/aeris-catalogue-components.js
+++ src/lib/aeris-catalogue-components.js
@@ -44,35 +44,6 @@ import AerisParameterSearchCriteria from "./components/aeris-catalog-criteria/ae
 import AerisProjectSearchCriteria from "./components/aeris-catalog-criteria/aeris-search-criteria/aeris-project-search-criteria/aeris-project-search-criteria.vue";
 import AerisThesaurusItemTreeCheckboxLayout from "./components/aeris-catalog-layouts/aeris-search-criteria-layout/aeris-thesaurus-item-tree-checkbox-layout.vue";
 
-var css = [
-  "https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/0.7.0/css/perfect-scrollbar.min.css",
-  "https://cdnjs.cloudflare.com/ajax/libs/ol3/4.2.0/ol.css",
-  "https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/themes/prism-okaidia.css"
-];
-var js = [
-  "https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/0.7.0/js/perfect-scrollbar.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/ol3/4.2.0/ol-debug.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/prism.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/components/prism-bash.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.js"
-];
-
-var i;
-for (i = 0; i < css.length; i++) {
-  var link = document.createElement("link");
-  link.setAttribute("type", "text/css");
-  link.setAttribute("rel", "stylesheet");
-  link.setAttribute("href", css[i]);
-  document.head.appendChild(link);
-}
-
-for (i = 0; i < js.length; i++) {
-  var script = document.createElement("script");
-  script.setAttribute("type", "text/javascript");
-  script.setAttribute("src", js[i]);
-  document.head.appendChild(script);
-}
-
 var components = [
   AerisUiInput,
   AerisUiCheckbox,
