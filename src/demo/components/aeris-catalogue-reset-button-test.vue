1b9ce7538ab08cc572ce4feac67a949102a13b7b
Samir Boumaza <boumaza-samir@hotmail.fr>
2019-02-15 09:18:48 +0100

    refactor(catalogue):demo style change & var rename

--- src/demo/components/aeris-catalogue-reset-button-test.vue
+++ src/demo/components/aeris-catalogue-reset-button-test.vue
@@ -1,12 +1,10 @@
 <template>
-<div style="padding:40px">
-    <aeris-catalogue-reset-button lang="en" :theme="theme" @CatalogueReset="visible= !visible"></aeris-catalogue-reset-button>
-    <p v-if="visible">emit : reset des criteres éffectué</p>
-</div>
+  <div style="padding:40px">
+      <aeris-catalogue-reset-button lang="en" :theme="theme" @CatalogueReset="visible= !visible"></aeris-catalogue-reset-button>
+      <p v-if="visible">emit : reset des criteres éffectué</p>
+  </div>
 </template>
-
 <script>
-
 import AerisCatalogueResetButton from "../../lib/modules/aeris-catalog-buttons/aeris-catalogue-reset-button/components/aeris-catalogue-reset-button.vue";
 
 export default {
