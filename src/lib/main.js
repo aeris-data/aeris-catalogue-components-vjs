f4db204d0449a4bd9c76e2e5e02def50ca7f6ec9
Samir Boumaza <boumaza-samir@hotmail.fr>
2019-02-18 13:28:05 +0100

    refactor(catalogue):remove unused files

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,17 +1,9 @@
 import Vue from "vue";
 
-import vueCustomElement from "vue-custom-element";
-Vue.use(vueCustomElement);
-
 import VueI18n from "vue-i18n";
 Vue.use(VueI18n);
-
-import VueResource from "vue-resource";
-Vue.use(VueResource);
 
 import { VueColorPlugin, VueAerisLanguagePlugin } from "aeris-mixins";
 Vue.use(VueColorPlugin);
 Vue.use(VueAerisLanguagePlugin);
 
-import VueCustomElementRecorder from "./vue-custom-element-recorder.js";
-VueCustomElementRecorder.run();
