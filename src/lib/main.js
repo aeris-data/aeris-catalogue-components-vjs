51477ac751d5ace204e1ce9606657273ea664fd4
Samir Boumaza <boumaza-samir@hotmail.fr>
2019-03-04 15:55:15 +0100

    refactor(reset-text-button): remove git tag

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,17 +1,8 @@
 import Vue from "vue";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> refactoring
 import VueI18n from "vue-i18n";
 Vue.use(VueI18n);
 
 import { VueColorPlugin, VueAerisLanguagePlugin } from "aeris-mixins";
 Vue.use(VueColorPlugin);
 Vue.use(VueAerisLanguagePlugin);
-
-<<<<<<< HEAD
-=======
-
->>>>>>> refactoring
